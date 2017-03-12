f1b68910b848c37b7be3fb7d240c9f75c61a4c7b
Brian <riceb53@gmail.com>
2017-03-12 09:30:39 -0400

    Make create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -12,4 +12,19 @@ class RecipesController < ApplicationController
   def new
     render 'new.html.erb'
   end
+
+  def create
+    # make the actual recipe in the db
+    @recipe = Recipe.new(
+      title: params['form_title'],
+      chef: params['form_chef'],
+      ingredients: params['form_ingredients'],
+      directions: params['form_directions'],
+      image: params['form_image'],
+      prep_time: params['form_prep_time']
+      )
+    @recipe.save
+    # show them a view with the info of the recipe they just created
+    render 'create.html.erb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get '/' => 'recipes#index'
   get '/recipes' => 'recipes#index'
   get '/recipes/new' => 'recipes#new'
+  post '/recipes' => 'recipes#create'
   get '/recipes/:id' => 'recipes#show'
   # make a route that the form will post to
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
