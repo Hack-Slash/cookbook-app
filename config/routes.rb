e77142392d3c1d3b45eb87c0137f56d37087a9a1
Brian <riceb53@gmail.com>
2017-03-12 09:17:00 -0400

    Add new RESTFUL  action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get '/' => 'recipes#index'
+  # get '/' => 'recipes#index'
   get '/recipes' => 'recipes#index'
+  get '/recipes/new' => 'recipes#new'
   get '/recipes/:id' => 'recipes#show'
-
+  # make a route that the form will post to
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
