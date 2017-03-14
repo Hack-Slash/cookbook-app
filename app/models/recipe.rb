35ad7a0f81d2460f7d64d4ab2cd23fd895b02b8b
Brian <riceb53@gmail.com>
2017-03-14 11:30:52 -0400

    finish sprucing up. make model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ApplicationRecord
+  def directions_list
+    return directions.split(", ")
+  end
+
+  def ingredients_list
+    return ingredients.split(", ")
+  end
+
+  def friendly_created_at
+    return created_at.strftime("%A, %b %d")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hours " if hours > 0
+    result += "#{minutes} minutes" if minutes > 0
+    result
+  end
 end
